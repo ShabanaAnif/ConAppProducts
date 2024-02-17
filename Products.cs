48d3e100876e1ead7aa65c78779c5d0b375f4a82
shabana <J.maxwell@simplilearn.com>
2024-02-17 03:31:40 -0800

    first commit

---
+++ Products.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppProducts
+{
+    public class Products
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Brand { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
+        public Products(string productName, decimal price, string brand, DateTime manufacturingDate, DateTime expiryDate)
+        {
+            ProductName = productName;
+            Price = price;
+            Brand = brand;
+            ManufacturingDate = manufacturingDate;
+            ExpiryDate = expiryDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Product Name: {ProductName}\nPrice: {Price}\nBrand: {Brand}\nManufacturing Date: {ManufacturingDate.ToShortDateString()}\nExpiry Date: {ExpiryDate.ToShortDateString()}";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConAppProducts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating a product instance
+        Products product1 = new Products("Laptop", 999.99m, "BrandX", new DateTime(2023, 5, 15), new DateTime(2026, 5, 15));
+
+        // Displaying product details
+        Console.WriteLine(product1);
+    }
+}
